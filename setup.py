1419d3a15a08f25697b61b2d34e1bb2fa5ffd303
Jhon Yana <ramstojh@gmail.com>
2021-10-08 00:16:10 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='terra',
+      version='0.1',
+      description='Script to determine missing rocky material of a star',
+      url='https://github.com/ramstojh/terra',
+      author='Jhon Yana',
+      author_email='ramstojh@alumni.usp.br',
+      license='MIT',
+      packages=['terra'],
+      install_requires=['numpy', 'pandas', 'tqdm', 'astropy', 'matplotlib'],
+      zip_safe=False)
